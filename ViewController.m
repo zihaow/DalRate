4aced5ea0dbfb3f987903158bb6c5e50d32b6b94
zihaow <zihao.wu1993@gmail.com>
2015-03-08 18:34:39 -0300

    Adjust sizes for different devices
    Automatically adjust sizes for different devices: iPhone 4S, iPhone
    5/5S, iPhone 6/6 plus

--- ViewController.m
+++ ViewController.m
@@ -8,6 +8,7 @@
 
 #import "ViewController.h"
 #import "AppDelegate.h"
+#import "TFHpple.h" 
 
 @interface ViewController ()
 @property (weak, nonatomic) IBOutlet UITableView *link;
@@ -24,6 +25,7 @@
 
 
 //- (IBAction)titleAction:(id)sender;
+@property (strong, nonatomic) IBOutlet NSLayoutConstraint *height;
 
 
 
@@ -34,30 +36,50 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
+    [self setNeedsStatusBarAppearanceUpdate];
     
-
- 
-    //create the segment control
-    /**
-    NSArray *options = [NSArray arrayWithObjects:@"First Year", @"Second Year", @"Third Year",@"Fourth Year", nil];
-    UISegmentedControl* control = [[UISegmentedControl alloc] initWithItems:options];
-    control.momentary = NO;
-    //[[UISegmentedControl appearance] setContentPositionAdjustment:UIOffsetMake(0, 0) //forSegmentType:UISegmentedControlSegmentCenter barMetrics:UIBarMetricsDefault];
-    control.frame = CGRectMake(35, 320, 250, 35);
-    //add it to the navigation bar
-    self.navigationItem.titleView = control;
+    self.navigationController.navigationBar.barTintColor = [UIColor blackColor];
+    self.navigationController.navigationBar.topItem.title = @"Rate a course";
+    
+    NSDictionary *textAttributes = [NSDictionary dictionaryWithObjectsAndKeys:
+                                    [UIColor whiteColor],NSForegroundColorAttributeName,
+                                    [UIColor whiteColor],NSBackgroundColorAttributeName,nil];
+    
+    self.navigationController.navigationBar.titleTextAttributes = textAttributes;
+    //self.title = @"Title of the Page";
+    
+    //self.navigationController.navigationBar
+    /*
+    [self.navigationController.navigationBar setTitleTextAttributes:
+     [NSDictionary dictionaryWithObjectsAndKeys:
+      [UIFont fontWithName:@"mplus-1c-regular" size:50],
+      NSFontAttributeName, nil]];
     */
+    _link.backgroundColor = [UIColor blackColor];
     
- 
+    CGFloat screenHeight = [UIScreen mainScreen].bounds.size.height;
     
-   
+    /* if user device is iPhone 4S, then adjust tableview height for better viewing experience. */
+    if(screenHeight == 480){
+      _height.constant = 310;
+    }
     
+    /* if user device is iPhone 5 or iPhone 5S, then adjust tableview height for better viewing experience. */
+    if(screenHeight == 568){
+        _height.constant = 400;
+    }
+    
+    /* if user device is iPhone 6 plus, then adjust tableview height for better viewing experience. */
+    if(screenHeight == 736){
+        _height.constant = 565;
+        //NSLog(@"height is %f", screenHeight);
+    }
     
     _firstYear = [[NSArray alloc] initWithObjects:@"CSCI 1100", @"CSCI 1101", nil];
-    _secondYear = [[NSArray alloc] initWithObjects:@"CSCI 2110",@"CSCI 2112",@"CSCI 2121",@"CSCI 2132",@"CSCI 2141", nil];
-    _thirdYear = [[NSArray alloc] initWithObjects:@"CSCI 3110",@"CSCI 3120",@"CSCI 3130",@"CSCI 3154",@"CSCI 3171",nil];
-    _fourthYear = [[NSArray alloc] initWithObjects:@"CSCI 4116",@"CSCI 4140", @"CSCI 4171",@"CSCI 4174", nil];
-    _allYear = [[NSArray alloc] initWithObjects:@"CSCI 1100", @"CSCI 1101",@"CSCI 2110",@"CSCI 2112",@"CSCI 2121",@"CSCI 2132",@"CSCI 2141",@"CSCI 3110",@"CSCI 3120",@"CSCI 3130",@"CSCI 3154",@"CSCI 3171",@"CSCI 4116",@"CSCI 4140", @"CSCI 4171",@"CSCI 4174", nil];
+    _secondYear = [[NSArray alloc] initWithObjects:@"CSCI 2100",@"CSCI 2110",@"CSCI 2112",@"CSCI 2121",@"CSCI 2132",@"CSCI 2141", nil];
+    _thirdYear = [[NSArray alloc] initWithObjects:@"CSCI 3101",@"CSCI 3110",@"CSCI 3120",@"CSCI 3121",@"CSCI 3130",@"CSCI 3132",@"CSCI 3136",@"CSCI 3151",@"CSCI 3154",@"CSCI 3160",@"CSCI 3161",@"CSCI 3162",@"CSCI 3171",@"CSCI 3172",@"CSCI 3190",nil];
+    _fourthYear = [[NSArray alloc] initWithObjects:@"CSCI 4112",@"CSCI 4116",@"CSCI 4117",@"CSCI 4125",@"CSCI 4126",@"CSCI 4140", @"CSCI 4144",@"CSCI 4145",@"CSCI 4152",@"CSCI 4155",@"CSCI 4160",@"CSCI 4163",@"CSCI 4166",@"CSCI 4167",@"CSCI 4168",@"CSCI 4169",@"CSCI 4171",@"CSCI 4174",@"CSCI 4176",@"CSCI 4180",@"CSCI 4181", nil];
+    _allYear = [[NSArray alloc] initWithObjects:@"CSCI 1100", @"CSCI 1101",@"CSCI 2100",@"CSCI 2110",@"CSCI 2112",@"CSCI 2121",@"CSCI 2132",@"CSCI 2141",@"CSCI 3101",@"CSCI 3110",@"CSCI 3120",@"CSCI 3121",@"CSCI 3130",@"CSCI 3132",@"CSCI 3136",@"CSCI 3151",@"CSCI 3154",@"CSCI 3160",@"CSCI 3161",@"CSCI 3162",@"CSCI 3171",@"CSCI 3172",@"CSCI 3190",@"CSCI 4112",@"CSCI 4116",@"CSCI 4117",@"CSCI 4125",@"CSCI 4126",@"CSCI 4140", @"CSCI 4144",@"CSCI 4145",@"CSCI 4152",@"CSCI 4155",@"CSCI 4160",@"CSCI 4163",@"CSCI 4166",@"CSCI 4167",@"CSCI 4168",@"CSCI 4169",@"CSCI 4171",@"CSCI 4174",@"CSCI 4176",@"CSCI 4180",@"CSCI 4181", nil];
     
     
     self.showData = _allYear;
@@ -74,6 +96,7 @@
     static NSString *cellIdentifier = @"tableCell";
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellIdentifier forIndexPath:indexPath];
     cell.textLabel.text = [_showData objectAtIndex:indexPath.row];
+    cell.textLabel.textColor = [UIColor whiteColor];
     
     return cell;
 }
@@ -82,20 +105,6 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
-/**
-- (IBAction)titleAction:(id)sender {
-    [UIView animateWithDuration:0.1 animations:^{
-        self.title.transform = CGAffineTransformMakeTranslation(10, 0);}];
-}
- */
-
-
-
-
-
-
-
-
 
 - (IBAction)titleAction:(UISegmentedControl *)sender {
     switch (_navi.selectedSegmentIndex)
@@ -128,6 +137,19 @@
         default:
             break;
     }
+    
+    /* Animate the table view reload */
+    [UIView transitionWithView: self.link
+                      duration: 0.75f
+                       options: UIViewAnimationOptionTransitionCrossDissolve
+                    animations: ^(void)
+     {
+         [self.link reloadData];
+     }
+                    completion: ^(BOOL isFinished)
+     {
+         /* TODO: Whatever you want here */
+     }];
 }
 - (IBAction)topicMove:(UIButton *)sender {
     [UIView animateWithDuration:0.1 animations:^{
@@ -150,6 +172,7 @@
     delegate.SegmentID = self.SegmentID;
     NSIndexPath *path = [self.link indexPathForSelectedRow];
     delegate.CellID = path.row;
+    delegate.CellName = [NSString stringWithString:[_allYear objectAtIndex:path.row]];
     if(_SegmentID == [NSNumber numberWithInt:0]){
         delegate.CellName = [NSString stringWithString:[_allYear objectAtIndex:path.row]];
     }
@@ -166,6 +189,11 @@
         delegate.CellName = [NSString stringWithString:[_fourthYear objectAtIndex:path.row]];
         
     }
+}
+
+-(UIStatusBarStyle)preferredStatusBarStyle{
+    return UIStatusBarStyleLightContent;
+    //return UIStatusBarStyleDefault
 }
 
 @end
